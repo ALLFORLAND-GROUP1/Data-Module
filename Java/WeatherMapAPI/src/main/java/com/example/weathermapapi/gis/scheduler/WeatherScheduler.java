4c05245221d35c360a2d6131ec85cd6c694dfe19
syys1028 <syys10280@naver.com>
2025-12-06 22:10:01 +0900

    chore: DB 스케줄러 시간 조정

--- Java/WeatherMapAPI/src/main/java/com/example/weathermapapi/gis/scheduler/WeatherScheduler.java
+++ Java/WeatherMapAPI/src/main/java/com/example/weathermapapi/gis/scheduler/WeatherScheduler.java
@@ -14,7 +14,7 @@ public class WeatherScheduler {
     private final WeatherService weatherService;
 
     // 매일 새벽 4시에 기본 5개 역 48시간 예보 저장
-    @Scheduled(cron = "0 0 4 * * *", zone = "Asia/Seoul")
+    @Scheduled(cron = "0 5 4 * * *", zone = "Asia/Seoul")
     public void saveForecastDaily() {
         log.info("Scheduler 실행");
         weatherService.saveDataForStations();
